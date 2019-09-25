e8c8259175ba99dcb29240f01145aa9c73712496
Francisco Silva <franciscocpfsilva_fcp@hotmail.com>
2019-09-25 12:49:57 +0100

    Updates

---
+++ precache-manifest.9b84a0b2f15228029730da3e9cd2319f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a553444736d583ee7aaac1ff2e11accc",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "31bd549fa6a57bee655a",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5159e5f74fc1f4037353",
+    "url": "/robofriends/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "31bd549fa6a57bee655a",
+    "url": "/robofriends/static/js/2.c02826ae.chunk.js"
+  },
+  {
+    "revision": "5159e5f74fc1f4037353",
+    "url": "/robofriends/static/js/main.ce565ae5.chunk.js"
+  },
+  {
+    "revision": "241cc94848c23d3d9c12",
+    "url": "/robofriends/static/js/runtime-main.e343c184.js"
+  }
+]);
