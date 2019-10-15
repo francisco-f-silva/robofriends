66287c1a79aebdf0599809109450ffa1f8f5b33f
Francisco Silva <franciscocpfsilva_fcp@hotmail.com>
2019-10-15 13:17:56 +0100

    organized files into packages + did stylistic corrections

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-	
 	const cardsArray = robots.map((user, i) => {
 		return (
 			<Card 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'; // webpack is doing the bundle of these imports in the background
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
